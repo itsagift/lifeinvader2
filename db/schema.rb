3e1175c1f175569867a0f9cdc0e2318cab1e4d62
DO72 <michaellaw@gmx.com>
2022-05-18 11:59:40 -0400

    Added comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_154110) do
+ActiveRecord::Schema.define(version: 2022_05_18_155430) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
